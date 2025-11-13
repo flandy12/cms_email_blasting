8e15aa533b2349d78d9556aec62790901087ca9b
flandy12 <halloflandy@gmail.com>
2025-11-13 15:56:32 +0700

    master

---
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { home } from '@/routes';
+import { Link, usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+const name = page.props.name;
+const quote = page.props.quote;
+
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>();
+</script>
+
+<template>
+    <div
+        class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <slot name="left" />
+        <div class="lg:p-8">
+            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div class="flex flex-col space-y-2 text-center">
+                    <h1 class="text-xl font-medium tracking-tight" v-if="title">
+                        {{ title }}
+                    </h1>
+                    <p class="text-sm text-muted-foreground" v-if="description">
+                        {{ description }}
+                    </p>
+                </div>
+                <slot />
+            </div>
+        </div>
+        <slot name="right" />
+    </div>
+</template>
